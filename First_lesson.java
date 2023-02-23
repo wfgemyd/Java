a3d3ea8b7312485f4ef6520009e37f4e50d3dacf
daniel shutov <66516254+wfgemyd@users.noreply.github.com>
2023-02-23 05:48:56 -0800

    small fix

--- First_lesson.java
+++ First_lesson.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
 
+
 public class App {
     public static void main(String[] args) throws Exception {
     String temp =scan(args);
     char array[] = new char[temp.length()];
     array = temp.toCharArray();
+    int sum = 0;
     for(int i = 0; i < temp.length(); i++){
         System.out.println("This is you ascii value of: "+array[i]+' '+(int)array[i]);
-        
+        sum+=(int)array[i];
     }
+    System.out.println("This is your sum: "+sum);
     }
 
     public static String scan(String[] args) {
         Scanner temp = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Enter your character");
-    
         String x = temp.nextLine();  // Read user input
         return x;
     }
